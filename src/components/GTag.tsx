5c5eb5e0938679d36f4cf1bc9d7cea29ad31c595
JexLau <jexlau.dev@gmail.com>
2024-02-17 00:27:43 +0800

    fix: gtag

--- src/components/GTag.tsx
+++ src/components/GTag.tsx
@@ -2,11 +2,11 @@ import Script from "next/script"
 
 export const GTag = () => {
   return <>
-    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-BM6KN3Y3PE"></Script>
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-KE5E4DQ1VV"></Script>
     <Script id="gtag" async dangerouslySetInnerHTML={{
       __html: `window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'G-BM6KN3Y3PE');`}} />
+  gtag('config', 'G-KE5E4DQ1VV');`}} />
   </>
 }

8be0550edd24b55a901c28faa044cc3a67206914
JexLau <jexlau.dev@gmail.com>
2024-02-23 16:48:40 +0800

    fix: meta property

--- src/components/Meta/index.tsx
+++ src/components/Meta/index.tsx
@@ -31,7 +31,7 @@ export const Meta = ({ title, description, keywords, locale = 'en', pageurl, ima
     <meta property="og:title" content={title} />
     <meta property="og:type" content="website" />
     <meta property="og:site_name" content={"Sora Maker AI"} />
-    <meta name="og:image" content={image} />
+    <meta property="og:image" content={image} />
     <meta property="og:url" content={url} />
     <meta property="og:description" content={description} />
     <meta property="og:locale" content={locale} />
